d9b4a465ab9902c50f11a553c01cd806d3423ed1
Rafael Ormazabal <riomazabal@uc.cl>
2021-06-18 19:13:29 -0400

    funciona el algoritmo de desicion

--- 2021-1/Capstone/Entrega 5/Nueva_Referencia/nueva_ref.py
+++ 2021-1/Capstone/Entrega 5/Nueva_Referencia/nueva_ref.py
@@ -14,11 +14,11 @@ def nueva_ref(referencias,index_ref,pos_x,pos_y,theta,distancias):
     q_2=float(distancias[1][1])/180.0*np.pi
 
     ##posicion punto más cercano
-    x1= pos_x + np.cos(theta - q_1)*l_1
-    y1= pos_y + np.sin(theta - q_1)*l_1
+    x1= pos_x + np.cos(theta + q_1)*l_1
+    y1= pos_y + np.sin(theta + q_1)*l_1
     ##posicion extremo más cercano
-    x2= pos_x + np.cos(theta - q_2)*l_2
-    y2= pos_y + np.sin(theta - q_2)*l_2
+    x2= pos_x + np.cos(theta + q_2)*l_2
+    y2= pos_y + np.sin(theta + q_2)*l_2
 
     #norma vector p2-p1
     norma=np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
@@ -27,7 +27,34 @@ def nueva_ref(referencias,index_ref,pos_x,pos_y,theta,distancias):
     x3=x2 + (x2 - x1)/norma*200.0
     y3=y2 + (y2 - y1)/norma*200.0
     #colocar nueva referencia
-    refs_x.insert((index_ref-1), x3)
-    refs_y.insert((index_ref-1), y3)
+    refs_x.insert((index_ref - 1), x3)
+    refs_y.insert((index_ref - 1), y3)
 
-    return [refs_x,refs_y]
+    #eliminar las próximas n referencias
+    n=1
+    #generar copias
+    refs_x_aux_1=refs_x.copy()
+    refs_y_aux_1=refs_y.copy()
+    ##cortar
+    ##falta revisar casos borde
+    refs_x_aux_1=refs_x_aux_1[0:(index_ref )]
+    refs_x_aux_2=refs_x[(index_ref + n ):(-1)]
+    refs_y_aux_1=refs_y_aux_1[0:(index_ref )]
+    refs_y_aux_2=refs_y[(index_ref + n ):(-1)]
+    ##unir listas
+    refs_x_aux_1.extend(refs_x_aux_2)
+    refs_y_aux_1.extend(refs_y_aux_2)
+    
+
+
+    #retornar listas
+    return [refs_x_aux_1,refs_y_aux_1]
+
+refs=[[200.0, 400.0, 600.0, 800.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0000000000001, 800.0, 600.0, 400.0, 200.0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 200.0, 400.0, 600.0, 800.0, 1000.0, 1000.0, 1000.0, 1000.0000000000001, 1000.0000000000001, 1000.0000000000001, 800.0, 600.0, 400.0, 200.0, 0]]
+index_ref=3
+posx=400.0
+posy=0.0
+theta=0.0
+dists=[[14.1,0.0],[17.286,35.34]]
+
+print(nueva_ref(refs,index_ref,posx,posy,theta,dists))
---
+++ 2021-1/Capstone/Entrega 5/Prueba_Algoritmo_Desicion/Prueba.m
@@ -0,0 +1,60 @@
+%%calcular extremos del obstaculo
+obs0=[541.0 0];
+largo0=100.0;
+largo1=150.0;
+angle=90.0;
+%%extremo 1
+aux1=obs0(1)+largo0*cos(angle*pi/180);
+aux2=obs0(2)+largo0*sin(angle*pi/180);
+obs1=[aux1 aux2];
+%%extremo 2
+aux1=obs0(1)-largo1*cos(angle*pi/180);
+aux2=obs0(2)-largo1*sin(angle*pi/180);
+obs2=[aux1 aux2];
+
+%%agregar puntos entremedio
+i=1;
+obs_1_0=[];
+obs_1_1=[];
+while  ((i*10.0)<=largo0)
+    aux1=obs1(1)-i*10.0*cos(angle*pi/180);
+    aux2=obs1(2)-i*10.0*sin(angle*pi/180);
+    obs_1_0=[obs_1_0 aux1];
+    obs_1_1=[obs_1_1 aux2];
+    i=i+1;
+end
+
+i=1;
+obs_2_0=[];
+obs_2_1=[];
+while  ((i*10.0)<=largo1)
+    aux1=obs2(1)+i*10.0*cos(angle*pi/180);
+    aux2=obs2(2)+i*10.0*sin(angle*pi/180);
+    obs_2_0=[obs_2_0 aux1];
+    obs_2_1=[obs_2_1 aux2];
+    i=i+1;
+end
+
+%%lista de puntos del obstaculo
+x=[obs0(1) obs_1_0 obs1(1) obs_2_0 obs2(1)];
+y=[obs0(2) obs_1_1 obs1(2) obs_2_1 obs2(2)];
+
+%scatter(x,y,'filled')
+plot(x,y)
+hold on
+
+%%vehiculo
+pos_x=400;
+pos_y=0;
+l_1=obs0(1)-pos_x;
+q_1=0;
+l_2=sqrt((obs1(1)-pos_x)^2+(obs1(2)-pos_y)^2);
+q_2=atan((obs1(2)-pos_y)/(obs1(1)-pos_x));
+
+posx=[pos_x , 541.02,800.0];
+posy=[pos_y , 299.99 ,0];
+scatter(posx,posy,'filled')
+
+hold off
+
+
